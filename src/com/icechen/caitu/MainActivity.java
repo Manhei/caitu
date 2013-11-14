6aa1cf2da25dbd20ce492472be7ddf1695274f0a
sunbo0123 <xy3548286@qq.com>
2013-11-14 15:06:37 +0800

    Lgame Creat

--- src/com/icechen/caitu/MainActivity.java
+++ src/com/icechen/caitu/MainActivity.java
@@ -1,22 +1,55 @@
 package com.icechen.caitu;
 
+import org.loon.framework.android.game.LGameAndroid2DActivity;
+
+import com.icechen.caitu.sreen.MenuScreen;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends LGameAndroid2DActivity {
+
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public void onGamePaused() {
+		// TODO 自动生成的方法存根
+		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onGameResumed() {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public void onMain() {
+        /* 设置游戏是否横屏 */  
+		   
+        this.initialization(true);  
+
+          
+        /* 给Activity赋予一个画面，有点类似于View */  
+
+        this.setScreen(new MenuScreen(this));  
+
+          
+
+        this.setFPS(30);  
+
+          
+
+        this.setShowFPS(true);  
+
+          
+
+        this.setShowLogo(false);  
+
+          
+
+        this.showScreen();  
+
 	}
 
 }
